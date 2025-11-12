46c7958700532d9253922c89d8d3f3f83f6e35cc
fadelm2 <fadelmuhammad128@gmail.com>
2025-11-12 12:46:13 +0700

    pasang authentication in client

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ToastContainer } from "react-toastify";
+import {ClerkProvider} from "@clerk/nextjs";
 
 
 const geistSans = Geist({
@@ -27,6 +28,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
+        <ClerkProvider>
+
         <html lang="en">
         <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -39,5 +42,6 @@ export default function RootLayout({
         <ToastContainer position="bottom-right" />
         </body>
         </html>
+        </ClerkProvider>
     );
 }

6af3b2724fbc22342ab43b64feacd6d1b4c73812
fadelm2 <fadelmuhammad128@gmail.com>
2025-11-13 10:37:24 +0700

    add all midlleware

--- apps/payment-service/src/middleware/authMiddleware.ts
+++ apps/payment-service/src/middleware/authMiddleware.ts
@@ -18,3 +18,28 @@ export const shouldBeUser = createMiddleware<{
 
     await next();
 });
+
+
+export const shouldBeAdmin = createMiddleware<{
+    Variables: {
+        userId: string;
+    };
+}>(async (c, next) => {
+    const auth = getAuth(c);
+
+    if (!auth?.userId) {
+        return c.json({
+            message: "You are not logged in.",
+        });
+    }
+
+    const claims = auth.sessionClaims as CustomJwtSessionClaims;
+
+    if (claims.metadata?.role !== "admin") {
+        return c.json({ message: "Unauthorized!" });
+    }
+
+    c.set("userId", auth.userId);
+
+    await next();
+});

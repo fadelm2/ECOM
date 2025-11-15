59994e7384d41e0b0685b0cd545bc234b910d91a
fadelm2 <fadelmuhammad128@gmail.com>
2025-11-15 15:04:39 +0700

    users list

--- apps/admin/src/app/(dashboard)/orders/columns.tsx
+++ apps/admin/src/app/(dashboard)/orders/columns.tsx
@@ -14,17 +14,18 @@ import { cn } from "@/lib/utils";
 import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown, MoreHorizontal } from "lucide-react";
 import Link from "next/link";
+import {OrderType} from "@repo/types";
 
-export type Payment = {
-  id: string;
-  amount: number;
-  fullName: string;
-  userId: string;
-  email: string;
-  status: "pending" | "processing" | "success" | "failed";
-};
+// export type Payment = {
+//   id: string;
+//   amount: number;
+//   fullName: string;
+//   userId: string;
+//   email: string;
+//   status: "pending" | "processing" | "success" | "failed";
+// };
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<OrderType>[] = [
   {
     id: "select",
     header: ({ table }) => (
@@ -44,8 +45,8 @@ export const columns: ColumnDef<Payment>[] = [
     ),
   },
   {
-    accessorKey: "fullName",
-    header: "User",
+    accessorKey: "_id",
+    header: "ID",
   },
   {
     accessorKey: "email",
@@ -89,7 +90,7 @@ export const columns: ColumnDef<Payment>[] = [
       const formatted = new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD",
-      }).format(amount);
+      }).format(amount/100);
 
       return <div className="text-right font-medium">{formatted}</div>;
     },
@@ -97,7 +98,7 @@ export const columns: ColumnDef<Payment>[] = [
   {
     id: "actions",
     cell: ({ row }) => {
-      const payment = row.original;
+      const order = row.original;
 
       return (
         <DropdownMenu>
@@ -110,13 +111,13 @@ export const columns: ColumnDef<Payment>[] = [
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
             <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
+              onClick={() => navigator.clipboard.writeText(order._id)}
             >
               Copy payment ID
             </DropdownMenuItem>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
-              <Link href={`/apps/admin/src/app/(dashboard)/users/${payment.userId}`}>View customer</Link>
+              <Link href={`/apps/admin/src/app/(dashboard)/users/${order.userId}`}>View customer</Link>
             </DropdownMenuItem>
             <DropdownMenuItem>View payment details</DropdownMenuItem>
           </DropdownMenuContent>
